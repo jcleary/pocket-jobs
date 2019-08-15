6e50010ceee4f1f15f5def2e627c200b0cd38a1c
John Cleary <john@createk.io>
2019-08-15 07:04:39 -0400

    remove test folder as going to use rspec instead

--- test/controllers/payday_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class PaydayControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/bank_item_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class BankItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
