9ef89ed22493a8426478b0a66cc9e8285cb97ad4
John Cleary <john@createk.io>
2020-07-05 09:34:54 +0100

    track when a job type was last used

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,7 +3,15 @@ class Job < ApplicationRecord
   belongs_to :job_type
   belongs_to :payday, required: false
 
+  after_create :update_job_type
+
   scope :unpaid, -> { where(payday: nil) }
 
   accepts_nested_attributes_for :job_type
+
+  private
+
+  def update_job_type
+    job_type.update(last_used: Time.now)
+  end
 end
