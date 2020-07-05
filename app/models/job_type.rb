8521b190aae1cdddd563370db62f2320b78fd52b
John Cleary <john@createk.io>
2020-07-05 09:34:32 +0100

    add some validation

--- app/models/job_type.rb
+++ app/models/job_type.rb
@@ -3,6 +3,7 @@ class JobType < ApplicationRecord
   scope :custom, -> { where(custom: true) }
 
   validates :name, :points, presence: true
+  validates :points, numericality: { less_than: 40, greater_than: 0 }
 
   def name_with_points
     "#{name} (#{points.floor})"
