2676086d76d62e3337b517521e98d175724b01eb
John Cleary <john@createk.io>
2020-07-05 09:35:35 +0100

    upgrade custom jobs to the standard list if used frequently

--- app/models/job_type.rb
+++ app/models/job_type.rb
@@ -1,11 +1,23 @@
 class JobType < ApplicationRecord
+  has_many :jobs
+
   scope :standard, -> { where(custom: false) }
   scope :custom, -> { where(custom: true) }
+
+  before_save :upgrade_to_standard_job
 
   validates :name, :points, presence: true
   validates :points, numericality: { less_than: 40, greater_than: 0 }
 
   def name_with_points
     "#{name} (#{points.floor})"
+  end
+
+  private
+
+  def upgrade_to_standard_job
+    if custom? && jobs.count > 5
+      self.custom = false
+    end
   end
 end
