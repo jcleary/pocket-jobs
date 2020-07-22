b5cb33dfa3aea0e4dcd8c88dd0f25c489ac36fd6
John Cleary <john@createk.io>
2020-07-22 21:01:47 +0100

    Add ActiveAdmin

--- Gemfile
+++ Gemfile
@@ -23,6 +23,8 @@ gem 'cancancan'
 
 gem 'aws-sdk-sns'
 
+gem 'activeadmin'
+
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails'
--- db/seeds.rb
+++ db/seeds.rb
@@ -75,3 +75,4 @@ job_types.each do |points, names|
 end
 
 
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
